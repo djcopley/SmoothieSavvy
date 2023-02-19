b56fa982b56b94fd73417967b7020ab165322858
Daniel Copley <djcopley@users.noreply.github.com>
2023-02-19 13:44:01 -0500

    Initial Commit

---
+++ SmoothieSavvy/SmoothieSavvyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmoothieSavvyApp.swift
+//  SmoothieSavvy
+//
+//  Created by Daniel Copley on 2/19/23.
+//
+
+import SwiftUI
+
+@main
+struct SmoothieSavvyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
